1654f21749616e95515980a5871d974a0c7aea06
Gaston Valles <gastonvallesyeou@gmail.com>
2023-08-29 00:22:20 -0300

    Socket.io connections workinggit add .!

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,5 @@
 import express from 'express';
 import { Server as WebSocketServer } from 'socket.io';
-import http from 'http';
 import { createServer } from 'https';
 import { readFile } from 'fs/promises';
 import { setupSocketHandlers } from './sockets/socketHandlers';
@@ -8,27 +7,31 @@ import { setupSocketHandlers } from './sockets/socketHandlers';
 const startServer = async () => {
   const app = express();
 
-  const httpServer = http.createServer(app);
-
   const key = await readFile('./key.pem');
   const cert = await readFile('./cert.pem');
 
   const httpsServer = createServer({ key, cert }, app);
 
-  const io = new WebSocketServer(httpServer);
+  const io = new WebSocketServer(httpsServer);
 
   app.use(express.static(__dirname + '/public'));
+
+  // Agregar el endpoint GET en el path "/"
+  app.get('/', async (req, res) => {
+    const content = await readFile("public/chat.html")
+    res.writeHead(200, {
+      "content-type": "text/html"
+    });
+    res.write(content);
+    res.end();
+  });
 
   setupSocketHandlers(io);
 
   const port = process.env.PORT || 3000;
 
-  httpServer.listen(port, () => {
-    console.log(`HTTP Server listening at http://localhost:${port}`);
-  });
-
-  httpsServer.listen(443, () => {
-    console.log(`HTTPS Server listening at https://localhost:3001`);
+  httpsServer.listen(port, () => {
+    console.log(`HTTPS Server listening at https://localhost:${port}`);
   });
 };
 
