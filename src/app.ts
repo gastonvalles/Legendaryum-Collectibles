a18529e31d61ff9a9d99e9c88e3c6d7b8cfca94d
Gaston Valles <gastonvallesyeou@gmail.com>
2023-08-28 23:34:36 -0300

    Socket.io config and cert.pem and key.pem created

--- src/app.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { join } from 'path';
-
-const app = express();
-
-app.use(express.static(join(__dirname, '../public')));
-
-export { app };
--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,35 @@
-import { createServer } from 'http';
-import { app } from './app';
-import { Server } from 'socket.io';
+import express from 'express';
+import { Server as WebSocketServer } from 'socket.io';
+import http from 'http';
+import { createServer } from 'https';
+import { readFile } from 'fs/promises';
 import { setupSocketHandlers } from './sockets/socketHandlers';
 
-const server = createServer(app);
-const io = new Server(server);
+const startServer = async () => {
+  const app = express();
 
-setupSocketHandlers(io);
+  const httpServer = http.createServer(app);
 
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}`);
-});
+  const key = await readFile('./key.pem');
+  const cert = await readFile('./cert.pem');
+
+  const httpsServer = createServer({ key, cert }, app);
+
+  const io = new WebSocketServer(httpServer);
+
+  app.use(express.static(__dirname + '/public'));
+
+  setupSocketHandlers(io);
+
+  const port = process.env.PORT || 3000;
+
+  httpServer.listen(port, () => {
+    console.log(`HTTP Server listening at http://localhost:${port}`);
+  });
+
+  httpsServer.listen(443, () => {
+    console.log(`HTTPS Server listening at https://localhost:3001`);
+  });
+};
+
+startServer();
