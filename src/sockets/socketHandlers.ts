67cff1c5f5f07cf9675dc060d7fe1ea15f823b70
Gaston Valles <gastonvallesyeou@gmail.com>
2023-09-06 01:41:17 -0300

    Funcionalidad para agarrar las monedas funcionando a medias, faltan arreglos

--- src/sockets/socketHandlers.ts
+++ src/sockets/socketHandlers.ts
@@ -13,13 +13,30 @@ export const setupSocketHandlers = (io: Server): void => {
       console.log('Client disconnected:', socket.id);
     });
 
-    socket.on('joinRoom', async ({roomName, seed}) => {
+    const availableCoinsByRoom: Record<string, any[]> = {};
+
+    socket.on('joinRoom', async ({ roomName, seed }) => {
       console.log(`Client ${socket.id} joined:`, roomName);
 
       const roomConfig = rooms.find((room: any) => room.name === roomName);
       if (roomConfig) {
-        const coins = await generateCoinsForRoom(roomConfig, roomName, seed);
-        socket.emit('coinsData', coins);
+        if (!availableCoinsByRoom[roomName]) {
+          const coins = await generateCoinsForRoom(roomConfig, roomName, seed);
+          availableCoinsByRoom[roomName] = coins;
+          socket.emit('coinsData', coins);
+        } else {
+          socket.emit('coinsData', availableCoinsByRoom[roomName]);
+        }
+        socket.join(roomName);
+      }
+    });
+
+    socket.on('grabCoin', ({ roomName, coinId }) => {
+      const roomCoins = availableCoinsByRoom[roomName];
+      const coin = roomCoins.find((c) => c.id === coinId);
+      if (coin) {
+        coin.available = false;
+        io.to(roomName).emit('availableCoins', roomCoins.filter((c) => c.available));
       }
     });
   });
